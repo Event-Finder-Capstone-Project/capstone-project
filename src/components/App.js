e572a3e8f139a40c9cc8b2a6617162ff1aedbe74
Asia Skinner <wolfe.asia@gmail.com>
2023-08-01 14:26:40 -0700

    updating app.js code to match others

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,13 @@ import { auth } from "../firebase";
 import Signup from "./Auth/Signup";
 import Login from "./Auth/Login";
 import Signout from "./Auth/Signout";
-import NavBar from "./NavBar";
 import SingleEvent from "./Events/SingleEvent";
 import AllEvents from "./Events/AllEvents";
+import NavBar from "./NavBar";
 
 function App() {
   const [userLoggedIn, setUserLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
-
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setUserLoggedIn(!!user);
@@ -26,52 +25,47 @@ function App() {
     return () => unsubscribe();
   }, []);
   return (
-    <>
-      <Router>
-        <NavBar />
-        <Container>
-          {userLoggedIn ? (
-            <div className="w-100 mb-3">
-              <h2>Taskmaster - Asia </h2>
-              <h2>Gitmaster - Richie </h2>
-              <h2>Testmaster - Sarsh</h2>
-              <p>Coder - Fulong</p>
-              <Signout />
-              <Route path="/allevents/:id" element={<SingleEvent />} />
-                <Route path="/events" element={<AllEvents />} />
-            </div>
-          ) : (
+    <Router>
+      <NavBar />
+      <Container>
+        {userLoggedIn ? (
+          <div className="w-100 mb-3">
+            <h2>Taskmaster - Asia </h2>
+            <h2>Gitmaster - Richie </h2>
+            <h2>Testmaster - Sarsh</h2>
+            <p>Coder - Fulong</p>
+            <Signout />
+          </div>
+        ) : (
+          <div>
             <div>
-              <div>
-
-              
-                <h1 className="title">Event Finder</h1>
-
+              <h1 className="title">Event Finder</h1>
+            </div>
+            <div>
               <Routes>
                 <Route path="/" element={<Signup />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/allevents/:id" element={<SingleEvent />} />
+                <Route path="/events" element={<AllEvents />} />
               </Routes>
+            </div>
+            <div className="front-bottom">
+              <div className="front-bottom-item">
+                <p>
+                  New User? <NavLink to="/signup">Sign Up</NavLink>
+                </p>
               </div>
-              <div className="front-bottom">
-                <div className="front-bottom-item">
-                  <p>
-                    New User? <NavLink to="/signup">Sign Up</NavLink>
-                  </p>
-                </div>
-                <div className="front-bottom-item">
-                  <p>
-                    Already have an account?{" "}
-                    <NavLink to="/login">Log In</NavLink>
-                  </p>
-                </div>
+              <div className="front-bottom-item">
+                <p>
+                  Already have an account? <NavLink to="/login">Log In</NavLink>
+                </p>
               </div>
             </div>
-          )}
-        </Container>
-      </Router>
-    </>
+          </div>
+        )}
+      </Container>
+    </Router>
   );
 }
-
 export default App;
