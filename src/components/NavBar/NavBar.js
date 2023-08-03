f6471c4a1c86b5b3393b574ca766953ab1c79016
Asia Skinner <wolfe.asia@gmail.com>
2023-08-03 13:02:57 -0700

    Basic styling of Nav Bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,51 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../style/navbar.css";
+// import "../style/navbar.css";
 import Search from "./Search";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Image,
+  Row,
+  Col,
+} from "react-bootstrap";
 
 const NavBar = () => {
-
   return (
     <>
-    <div id="navbar" className="navbar">
-      <NavLink to="/" className="navlink">
-        Home
-      </NavLink>
-      <NavLink to="/myevents" className="navlink">
-        Your Events
-      </NavLink>
-      <NavLink to="/thisweekend" className="navlink">
-        This Weekend
-      </NavLink>
-      <NavLink to="/today" className="navlink">
-        Today
-      </NavLink>
-      <NavLink to="/free" className="navlink">
-        Free
-      </NavLink>
-      <NavLink to="/login" className="navlink">
-        Login
-      </NavLink>
-      <NavLink to="/signup" className="navlink">
-        Sign Up
-      </NavLink>
-      </div>
-      <Search />
-      </>
+      <Navbar bg="dark" expand="xxl" className="bg-body-tertiary">
+        <Container fluid="sm">
+          <Navbar.Brand href="/">
+            <Image src="favicon.ico" />
+          </Navbar.Brand>
+          <Nav.Link to="/" className="navlink">
+            Home
+          </Nav.Link>
+          <Nav.Link to="/myevents" className="navlink">
+            Your Events
+          </Nav.Link>
+          <Nav.Link to="/thisweekend" className="navlink">
+            This Weekend
+          </Nav.Link>
+          <Nav.Link to="/today" className="navlink">
+            Today
+          </Nav.Link>
+          <Nav.Link to="/free" className="navlink">
+            Free
+          </Nav.Link>
+          <Nav.Link to="/login" className="navlink">
+            Login
+          </Nav.Link>
+          <Nav.Link to="/signup" className="navlink">
+            Sign Up
+          </Nav.Link>
+
+          <Search />
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
