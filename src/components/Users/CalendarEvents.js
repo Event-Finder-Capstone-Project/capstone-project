6f689c3f5e028f2c441bf3eea54f5aab75d78919
richiepchavez <richiepchavez@gmail.com>
2023-08-04 09:46:34 -0700

    pushing map

--- src/components/Users/CalendarEvents.js
+++ src/components/Users/CalendarEvents.js
@@ -4,25 +4,27 @@ import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@
 const CalendarEvents = () => {
     const storedEvents = JSON.parse(localStorage.getItem('calendar')) || [];
     const [allEvents, setAllEvents] = React.useState(storedEvents);
+
+
     const handleActionBegin = (args) => {
-      // Clone the existing events array to avoid directly modifying the state array
       const updatedEvents = {...allEvents};
       if (args.requestType === 'eventCreate') {
         const newEvent = args.data[0];
-        updatedEvents[newEvent.Id] = newEvent; // Store the newly created event with its ID as the key
+        updatedEvents[newEvent.Id] = newEvent;
         console.log(newEvent);
       } else if (args.requestType === 'eventRemove') {
-        delete updatedEvents[args.data[0].Id]; // Remove the deleted event from the object using its ID
+        delete updatedEvents[args.data[0].Id];
       } else if (args.requestType === 'eventChange') {
         const updatedEvent = args.data[0];
-        updatedEvents[updatedEvent.Id] = updatedEvent; // Update the modified event in the object
+        updatedEvents[updatedEvent.Id] = updatedEvent; 
       }
       console.log('Updated Events:', updatedEvents);
-      setAllEvents(updatedEvents); // Update the state with the new array of events
+      setAllEvents(updatedEvents);
       localStorage.setItem('calendar', JSON.stringify
       (updatedEvents));
       console.log(allEvents)
     };
+
   return (
     <div>
       <ScheduleComponent width='70%' height='450px' currentView='Month'
