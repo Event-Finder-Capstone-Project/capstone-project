e682547282ef13825fc74820a011629d210d3531
Asia Skinner <wolfe.asia@gmail.com>
2023-08-08 13:16:27 -0700

    quick columnn change

--- src/components/Events/AllEvents.js
+++ src/components/Events/AllEvents.js
@@ -113,7 +113,7 @@ const AllEvents = () => {
       // For guest users, add the event to local storage
       dispatch(addEvents(eventId));
     }
-    setClickedEvents((prevClicked) => [...prevClicked, eventId]);
+    setClickedEvents([...clickedEvents, eventId]);
   };
 
   const handleFilter = () => {
@@ -211,12 +211,17 @@ const AllEvents = () => {
                     </Card.Body>
                   </Nav.Link>
                 </LinkContainer>
-                {!clickedEvents.includes(event.id) && (
+                {!clickedEvents.includes(event.id) &&
+                !userEvents.includes(event.id) ? (
                   <Button
                     variant="secondary"
                     onClick={() => handleAddEvents(event.id)}
                   >
                     Add Event
+                  </Button>
+                ) : (
+                  <Button variant="secondary" disabled>
+                    Event Added
                   </Button>
                 )}
               </Card>
