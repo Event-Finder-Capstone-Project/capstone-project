c135484a76b18922a4ba8097bfdee6e7afd7edc4
Asia Skinner <wolfe.asia@gmail.com>
2023-08-09 09:28:05 -0700

    had another thing to merge

--- src/components/Maps/TestMap.js
+++ src/components/Maps/TestMap.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { 
-  GoogleMap, 
-  useLoadScript, 
-  MarkerF, 
-  InfoWindowF
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  InfoWindowF,
 } from "@react-google-maps/api";
 import { Container } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllEvents, selectEvents } from "../../store/allEventsSlice";
-import '../style/index.css'
-
+import "../style/index.css";
 
 export default function TestMap() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyDrusDlQbaU-_fqPwkbZfTP1EMDzvQMGWU",
-    libraries:['places'],
+    libraries: ["places"],
   });
 
   const events = useSelector(selectEvents);
@@ -28,9 +27,8 @@ export default function TestMap() {
         <h4>Map Loading</h4>
       </Container>
     );
-<<<<<<< HEAD
   return (
-    <Container style={{ width: "119%", marginLeft: "-2.2rem" }}>
+    <Container style={{ width: "120%", marginLeft: "-2.2rem" }}>
       <GoogleMap
         zoom={10}
         center={{ lat: latitude, lng: longitude }}
@@ -43,47 +41,25 @@ export default function TestMap() {
               lat: marker.venue.location.lat,
               lng: marker.venue.location.lon,
             }}
+            onClick={() => setSelectedEvent(marker)}
           />
         ))}
+        {selectedEvent && (
+          <InfoWindowF
+            position={{
+              lat: selectedEvent.venue.location.lat,
+              lng: selectedEvent.venue.location.lon,
+            }}
+            onCloseClick={() => setSelectedEvent(null)}
+          >
+            <div className="custom-infowindow-content">
+              <h6>{selectedEvent.title}</h6>
+              <p>{selectedEvent.venue.name}</p>
+              <p>{selectedEvent.venue.city}</p>
+            </div>
+          </InfoWindowF>
+        )}
       </GoogleMap>
     </Container>
   );
 }
-=======
-    return (
-      <Container style={{ width: "120%", marginLeft: "-2.2rem" }}>
-        <GoogleMap
-          zoom={10}
-          center={{ lat: latitude, lng: longitude }}
-          mapContainerStyle={{ width: "100%", height: 350 }}
-        >
-          {events.map((marker) => (
-            <MarkerF
-              key={marker.id}
-              position={{
-                lat: marker.venue.location.lat,
-                lng: marker.venue.location.lon,
-              }}
-              onClick={() => setSelectedEvent(marker)}
-            />
-          ))}
-         {selectedEvent && (
-  <InfoWindowF 
-    position={{
-      lat: selectedEvent.venue.location.lat,
-      lng: selectedEvent.venue.location.lon,
-    }}
-    onCloseClick={() => setSelectedEvent(null)}
-  >
-    <div className="custom-infowindow-content">
-      <h6>{selectedEvent.title}</h6>
-      <p>{selectedEvent.venue.name}</p>
-      <p>{selectedEvent.venue.city}</p>
-    </div>
-  </InfoWindowF>
-)}
-        </GoogleMap>
-      </Container>
-    );
-  }
->>>>>>> 6fd67bcbfe64b2e8e732584200900a8ab1010776
