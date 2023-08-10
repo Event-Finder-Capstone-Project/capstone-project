bd16de8d8bc793b241d5f3244f8ffd0116b28b5e
Asia Skinner <wolfe.asia@gmail.com>
2023-08-10 09:29:13 -0700

    new carousel working and lookin snazzy

--- src/components/Events/NewCarousel.js
+++ src/components/Events/NewCarousel.js
@@ -1,4 +1,4 @@
-import { Nav, Carousel } from "react-bootstrap";
+import { Nav, Carousel, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -11,20 +11,31 @@ const NewCarousel = () => {
     .slice(0, 4);
 
   return (
-    <Carousel>
-      {sortedEvents.map((event, index) => (
-        <Carousel.Item key={index}>
-          {/* <LinkContainer to={`/events/${event.id}`}>
-            <Nav.Link> */}
-          <img src={event.performers[0].image} alt={`${index + 1}`} />
-          <Carousel.Caption style={{ color: "white" }}>
-            <h4 className="legend">{event.title}</h4>
-          </Carousel.Caption>
-          {/* </Nav.Link>
-          </LinkContainer> */}
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Container>
+      <Carousel data-bs-theme="dark">
+        {sortedEvents.map((event, index) => (
+          <Carousel.Item key={index}>
+            <LinkContainer
+              className="d-flex justify-content-center"
+              to={`/events/${event.id}`}
+            >
+              <Nav.Link>
+                <img
+                  style={{ width: "40%", height: "40%" }}
+                  src={event.performers[0].image}
+                  alt={`${index + 1}`}
+                />
+                <Carousel.Caption
+                  style={{ paddingLeft: "11%", color: "white", width: "59%" }}
+                >
+                  <h4 className="legend">{event.title}</h4>
+                </Carousel.Caption>
+              </Nav.Link>
+            </LinkContainer>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
   );
 };
 
