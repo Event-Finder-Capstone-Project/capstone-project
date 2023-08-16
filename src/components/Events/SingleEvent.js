35faa33d3bf4eec753aafc8af0e67010b0fcb714
richiepchavez <richiepchavez@gmail.com>
2023-08-15 21:08:09 -0700

    share events now passing in props

--- src/components/Events/SingleEvent.js
+++ src/components/Events/SingleEvent.js
@@ -123,7 +123,7 @@ const SingleEvent = () => {
                   Buy Tickets Here
                 </Button>
                 <BackButton />
-                <ShareEvent />
+                <ShareEvent eventId={event.id}/>
               </div>
             </Col>
           ) : null}
