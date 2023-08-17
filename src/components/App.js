fb69f2516e645aa7d435a84f5352d576ddb919eb
Asia Skinner <wolfe.asia@gmail.com>
2023-08-17 13:40:49 -0700

    made an update to pass props properly in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -88,9 +88,15 @@ function App() {
               />
               <Route path="/" element={<Home eventsData={eventsData} />} />
               <Route path="/user-profile" element={<UserProfile />} />
-              <Route path="/thisweekend" element={<Weekend />} />
+              <Route
+                path="/thisweekend"
+                element={<Weekend eventsData={eventsData} />}
+              />
               <Route path="/myevents" element={<UserEventsTwo />} />
-              <Route path="/today" element={<Today />} />
+              <Route
+                path="/today"
+                element={<Today eventsData={eventsData} />}
+              />
               <Route
                 path="/user-details"
                 element={<UserDetails user={user} />}
@@ -117,9 +123,15 @@ function App() {
                 />
                 <Route path="/events/:id" element={<SingleEvent />} />
                 <Route path="/myevents" element={<UserEventsTwo />} />
-                <Route path="/thisweekend" element={<Weekend />} />
+                <Route
+                  path="/thisweekend"
+                  element={<Weekend eventsData={eventsData} />}
+                />
                 <Route path="/" element={<Home eventsData={eventsData} />} />
-                <Route path="/searchresults" element={<SearchResults />} />
+                <Route
+                  path="/searchresults"
+                  element={<SearchResults eventsData={eventsData} />}
+                />
               </Routes>
             </div>
           </div>
